e052d0e70e4e96e1a7a66bfe066adeb3ff22c13a
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2021-11-13 14:05:50 +0200

    Add files via upload

---
+++ 5. Polymorphism/1. Polymorphism Lab/Math_Operation_01/Main.java
@@ -0,0 +1,7 @@
+package Math_Operation_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 5. Polymorphism/1. Polymorphism Lab/Shapes_02_2/Shape.java
@@ -0,0 +1,6 @@
+package Shapes_02_2;
+
+public interface Shape {
+    double calculatePerimeter();
+    double calculateArea();
+}
